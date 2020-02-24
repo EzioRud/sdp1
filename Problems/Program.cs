b8ced4a9985f043a4640dd6a4b7d2cc6249d4cf5
Luvhengo <tshilingadolph@gmail.com>
2020-02-24 21:31:50 +0200

    another commit

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Problems
 {
-    class Program
+    public class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Prefix("Hello World!"));
+
+        }
+
+        static string Prefix(string input)
+        {
+            if(input == null)
+            {
+                return null;
+            }
+            else if (input == "")
+            {
+                return "0,0" + ":" + input;
+            }
+            int iLength = input.Length;
+            string[] SplitArray = input.Split(" ");
+            int iWords = SplitArray.Length;
+            string answer = iLength.ToString() + "," + iWords.ToString() + ":" +input;
+
+            return answer;
         }
     }
 }
